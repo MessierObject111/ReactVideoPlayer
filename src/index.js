9f5f3a60dffeb9cc754d3f69bd63d078018396cc
ludwiglevo <ludwiglevo@gmail.com>
2017-10-21 23:57:02 -0400

    Adding search video by name feature.

--- src/index.js
+++ src/index.js
@@ -3,28 +3,44 @@ import ReactDOM from 'react-dom';
 import SearchBox from './components/search_box';
 import VideoList from './components/video_list';
 import VideoListItem from './components/video_list_item';
+import VideoDetail from './components/video_detail';
 
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCEZvUiWL3_AKX7hZuhyBejWNGZQgwI884';
+let searchWord = 'cats';
 
 // Create a new component which should produce some HTML
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {videos: []};
-
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch(searchWord);
+  };
+  videoSearch (term) {
+    YTSearch({key: API_KEY, term: searchWord}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
+  updateVideoSearch(word) => {
+    this.searchWord = word;
+  }
 
   render() {
     return (
       <div>
-        <SearchBox />
-        <VideoList videos={this.state.videos} />
+        <SearchBox onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail
+          video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
       </div>
     );
   }
